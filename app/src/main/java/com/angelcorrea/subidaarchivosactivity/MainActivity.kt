9011142d9936518fa95afcd8442c5d908bd351a3
AngelUPT <angel_1322134008@uptecamac.edu.mx>
2025-01-26 13:58:19 -0600

    adding actions

--- app/src/main/java/com/angelcorrea/subidaarchivosactivity/MainActivity.kt
+++ app/src/main/java/com/angelcorrea/subidaarchivosactivity/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.angelcorrea.subidaarchivosactivity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE_FILE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,37 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnSubirArchivo = findViewById<Button>(R.id.button)
+        val button = findViewById<Button>(R.id.button2)
+        btnSubirArchivo.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                type = "*/*"
+                addCategory(Intent.CATEGORY_OPENABLE)
+            }
+            startActivityForResult(Intent.createChooser(intent, "Selecciona un archivo"), REQUEST_CODE_FILE)
+        }
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Archivo seleccionado", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_FILE && resultCode == RESULT_OK) {
+            var selectedFileUri : Uri? = data?.data
+            if (selectedFileUri != null) {
+                Toast.makeText(
+                    this,
+                    "Archivo seleccionado: $selectedFileUri",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(this, "No se seleccionó ningún archivo", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
     }
 }
